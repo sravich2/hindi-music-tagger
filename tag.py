d64dad1b7b4e6a8c80d9cf8e3cf28a8b7d714938
Sachin <sravich2@illinois.edu>
2015-07-07 18:53:03 +0530

    Better strategy and duration added

--- tag.py
+++ tag.py
@@ -3,12 +3,17 @@ from __future__ import print_function
 __author__ = 'sachin'
 from bs4 import BeautifulSoup
 import urllib2
+import json
 
 def get_info(url):
     page = urllib2.urlopen(url).read()
     soup = BeautifulSoup(page, 'html.parser')
-    album_name = soup.find(class_='track-albumname').string
-    track_name = soup.find(class_='sngandalbum').contents[1].string
+    # print(soup.prettify())
+
+    track_info_json = json.loads(soup.find(class_='searchsonglist').string)
+    track_name = track_info_json['title']
+    album_name = track_info_json['albumtitle']
+    duration = secs_to_hm(track_info_json['duration'])
     artist_arr = []
     artists = soup.find(class_='artist').contents[1].contents
     for artist_tag in artists[1:len(artists):2]:
@@ -18,10 +23,17 @@ def get_info(url):
     print("Artists: " + artist_arr[0],end='')
     for artist in artist_arr[1:len(artist_arr)]:
         print(', ' + artist, end='')
+    print()
+    print("Duration: " + duration)
 
 def generate_url(title):
     modified_title = title.replace(' ', '+')
     return "http://www.gaana.com/search/songs/" + modified_title
+
+def secs_to_hm(seconds):
+    hour = int(seconds)/60
+    minutes = int(seconds)%60
+    return str(hour) + ":" + str(minutes)
 
 url = generate_url("mitwa")
 get_info(url)
