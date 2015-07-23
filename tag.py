f9d5efbe7a44abe2205bb823fce24f53881174e6
Sachin <sravich2@illinois.edu>
2015-07-23 15:22:20 +0530

    Using Saavn, major improvement

--- tag.py
+++ tag.py
@@ -1,40 +1,37 @@
 from __future__ import print_function
 
 __author__ = 'sachin'
-from bs4 import BeautifulSoup
-import urllib2
+import requests
+import re
 import json
-
+#
 def get_info(url):
-    page = urllib2.urlopen(url).read()
-    soup = BeautifulSoup(page, 'html.parser')
-    # print(soup.prettify())
+    r = requests.get(url)
+    matchObj = re.match(r'.*?<div class="hide song-json">(.*?)</div>', r.text, re.S)
+    track_info_json = json.loads(matchObj.group(1))
 
-    track_info_json = json.loads(soup.find(class_='searchsonglist').string)
     track_name = track_info_json['title']
-    album_name = track_info_json['albumtitle']
-    duration = secs_to_hm(track_info_json['duration'])
-    artist_arr = []
-    artists = soup.find(class_='artist').contents[1].contents
-    for artist_tag in artists[1:len(artists):2]:
-        artist_arr.append(artist_tag.string)
+    album_name = track_info_json['album']
+    duration = track_info_json['duration']
+    year = track_info_json['year']
+    artists = track_info_json['singers']
+    music_director = track_info_json['music']
+
     print("Title: " + track_name)
     print("Album: " + album_name)
-    print("Artists: " + artist_arr[0],end='')
-    for artist in artist_arr[1:len(artist_arr)]:
-        print(', ' + artist, end='')
-    print()
-    print("Duration: " + duration)
+    print("Artists: " + artists)
+    print("Music Director: " + music_director)
+    print("Duration: " + secs_to_hm(int(float(duration))))
+    print("Year: " + year)
 
 def generate_url(title):
-    modified_title = title.replace(' ', '+')
-    return "http://www.gaana.com/search/songs/" + modified_title
+    modified_title = title.replace(' ', '-')
+    return "http://www.saavn.com/s/" + modified_title
 
 def secs_to_hm(seconds):
-    hour = int(seconds)/60
-    minutes = int(seconds)%60
-    return str(hour) + ":" + str(minutes)
+    hour = int(seconds/60)
+    minutes = int(seconds%60)
+    return str(hour) + ":" + '{:02d}'.format(minutes)
 
-url = generate_url("mitwa")
+url = generate_url('enna solla pogirai kandukondain')
 get_info(url)
-
