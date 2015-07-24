77a8037b67a4ba1a84282362255c6dd7ce159720
Sachin <sravich2@illinois.edu>
2015-07-24 18:49:53 +0530

    All match

--- tag.py
+++ tag.py
@@ -4,25 +4,14 @@ __author__ = 'sachin'
 import requests
 import re
 import json
-#
+import HTMLParser
+
 def get_info(url):
     r = requests.get(url)
-    matchObj = re.match(r'.*?<div class="hide song-json">(.*?)</div>', r.text, re.S)
-    track_info_json = json.loads(matchObj.group(1))
+    matches = re.findall(r'.*?<div class="hide song-json">(.*?)</div>', r.text, re.S)
 
-    track_name = track_info_json['title']
-    album_name = track_info_json['album']
-    duration = track_info_json['duration']
-    year = track_info_json['year']
-    artists = track_info_json['singers']
-    music_director = track_info_json['music']
-
-    print("Title: " + track_name)
-    print("Album: " + album_name)
-    print("Artists: " + artists)
-    print("Music Director: " + music_director)
-    print("Duration: " + secs_to_hm(int(float(duration))))
-    print("Year: " + year)
+    for json_info in matches:
+        print_data_from_json(json_info)
 
 def generate_url(title):
     modified_title = title.replace(' ', '-')
@@ -33,5 +22,21 @@ def secs_to_hm(seconds):
     minutes = int(seconds%60)
     return str(hour) + ":" + '{:02d}'.format(minutes)
 
-url = generate_url('tere naina')
+def print_data_from_json(track_info):
+    track_info_json = json.loads(track_info)
+    track_name = HTMLParser.HTMLParser().unescape(track_info_json['title'])
+    album_name = HTMLParser.HTMLParser().unescape(track_info_json['album'])
+    duration = track_info_json['duration']
+    year = track_info_json['year']
+    artists = track_info_json['singers']
+    music_director = track_info_json['music']
+
+    print("Title: " + track_name)
+    print("Album: " + album_name)
+    print("Artists: " + artists)
+    print("Music Director: " + music_director)
+    print("Duration: " + secs_to_hm(int(float(duration))))
+    print("Year: " + year + "\n\n")
+
+url = generate_url('mitwa')
 get_info(url)
