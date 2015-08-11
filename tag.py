530bf54563b9357c2f9ec8e04c9ff7a4c2ec19b2
Sachin <sravich2@illinois.edu>
2015-08-11 21:23:03 +0530

    Lyrics added

--- tag.py
+++ tag.py
@@ -5,22 +5,28 @@ import requests
 import re
 import json
 import HTMLParser
+import mutagen
+import mutagen.easyid3
+
 
 def get_info(url):
     r = requests.get(url)
     matches = re.findall(r'.*?<div class="hide song-json">(.*?)</div>', r.text, re.S)
 
-    for json_info in matches:
+    for json_info in matches[0:2]:
         print_data_from_json(json_info)
+
 
 def generate_url(title):
     modified_title = title.replace(' ', '-')
     return "http://www.saavn.com/s/" + modified_title
 
+
 def secs_to_hm(seconds):
-    hour = int(seconds/60)
-    minutes = int(seconds%60)
+    hour = int(seconds / 60)
+    minutes = int(seconds % 60)
     return str(hour) + ":" + '{:02d}'.format(minutes)
+
 
 def print_data_from_json(track_info):
     track_info_json = json.loads(track_info)
@@ -30,13 +36,25 @@ def print_data_from_json(track_info):
     year = track_info_json['year']
     artists = track_info_json['singers']
     music_director = track_info_json['music']
+    lyrics = get_lyrics(track_info_json)
 
     print("Title: " + track_name)
     print("Album: " + album_name)
     print("Artists: " + artists)
     print("Music Director: " + music_director)
     print("Duration: " + secs_to_hm(int(float(duration))))
-    print("Year: " + year + "\n\n")
+    print("Year: " + year)
+    print("Lyrics: \n" + lyrics + "\n\n")
 
-url = generate_url('mitwa')
+
+def get_lyrics(track_info_json):
+    song_url = track_info_json['perma_url']
+    r = requests.get(song_url)
+    match_obj = re.match(r'.*?<h2 class="page-subtitle">Lyrics</h2>\s*<p>\s*(.*?)\s*</p>', r.text, re.S)
+    lyrics = match_obj.group(1)
+    lyrics = lyrics.replace('<br>', '\n')
+    return lyrics
+
+
+url = generate_url('kabhi alvida na kehna')
 get_info(url)
